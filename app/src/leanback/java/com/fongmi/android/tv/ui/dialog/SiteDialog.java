d4695ce98972cab4865d9ea6dc401c00c2f93bce
llb0 <108737112+llb0@users.noreply.github.com>
2026-08-11 21:42:47 +0800

    Add files via upload

--- app/src/leanback/java/com/fongmi/android/tv/ui/dialog/SiteDialog.java
+++ app/src/leanback/java/com/fongmi/android/tv/ui/dialog/SiteDialog.java
@@ -27,6 +27,7 @@ import com.fongmi.android.tv.bean.Site;
 import com.fongmi.android.tv.databinding.DialogSiteBinding;
 import com.fongmi.android.tv.impl.Callback;
 import com.fongmi.android.tv.impl.SiteListener;
+import com.fongmi.android.tv.R;
 import com.fongmi.android.tv.ui.adapter.SiteAdapter;
 import com.fongmi.android.tv.ui.custom.SpaceItemDecoration;
 import com.fongmi.android.tv.utils.Notify;
