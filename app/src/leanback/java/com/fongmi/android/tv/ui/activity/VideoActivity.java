29aaab49e7a9a0228104fbc8e580e66c6061aeca
llb0 <108737112+llb0@users.noreply.github.com>
2026-08-17 14:21:08 +0800

    Fix method call to get player type in saveHistory

--- app/src/leanback/java/com/fongmi/android/tv/ui/activity/VideoActivity.java
+++ app/src/leanback/java/com/fongmi/android/tv/ui/activity/VideoActivity.java
@@ -3561,7 +3561,7 @@ public class VideoActivity extends PlaybackActivity implements CustomKeyDownVod.
         if (mHistory == null || Setting.isIncognito()) return;
         if (service() != null && isOwner()) {
             updatePlaybackHistoryPosition();
-            mHistory.setPlayerType(player().playerType);
+            mHistory.setPlayerType(player().getPlayerType());
             mHistory.setCreateTime(System.currentTimeMillis());
         }
         if (exit && service() != null) PlaybackEventCollector.get().onStop(player());
